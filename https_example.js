608cbe6100eec2a9f66c84986a473be3fdc86a87
Chris kim <chriskihyun@hotmail.com>
2018-05-12 22:37:34 +0000

    first commit

--- https_example.js
+++ https_example.js
@@ -10,7 +10,11 @@ var options = {
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
@@ -20,3 +24,4 @@ https.request(options, callback).end();
 console.log("I've made the request!");
 
 // called by https when the request is made.
+
